9bd22a1390c99d6adaea0c9d74e5a43177dc0289
EvanHocking <evanhocking@ufl.edu>
2022-10-16 16:18:49 -0400

    machinegun
    rough machinegun system implementation. Code needs a bit of refactoring, but works. Added debug tools for the machinegun also.

--- Assets/Scripts/Weapons/bulletProjectiles.cs
+++ Assets/Scripts/Weapons/bulletProjectiles.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class bulletProjectiles : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float range = 0.0f;
+    private float damage = 0.0f;
+    private Vector3 startPosition = new Vector3();
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //calculate distance traveled
+        float distance = Mathf.Sqrt(Mathf.Pow((transform.position.x - startPosition.x), 2) + Mathf.Pow((transform.position.y - startPosition.y), 2));
+        if (distance > range)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    public void setStats(float _range, float _damage)
+    {
+        range = _range;
+        damage = _damage;
     }
 }
--- Assets/Scripts/Weapons/bulletWeapon.cs
+++ Assets/Scripts/Weapons/bulletWeapon.cs
@@ -7,7 +7,7 @@ public class bulletWeapon : MonoBehaviour
 {
     //Bullet aspects that can change (defaults)
     private float fireRate = 10.0f;               //Rounds per second
-    private float fireSpread = 1.0f;        //Angle of spread in radians
+    private float fireSpread = 0.2f;        //Angle of spread in radians
     private float projectileSpeed = 10.0f;   //Projectile speed in units/second
     private float projectileDamage = 1.0f;       //Damage each projectile deals
     private float projectileRange = 20.0f;   //Projectile range until destroyed
@@ -27,6 +27,10 @@ public class bulletWeapon : MonoBehaviour
     private bool debug = false;
     private float sumDeltaTime = 0.0f;
 
+    //weapon type flags (behavior flags)
+    public bool isSniper = false;
+    public bool isMachinegun = false;
+    public bool isShotgun = false;
     void Start()
     {
         weaponStats.Add(fireRate);
@@ -47,7 +51,6 @@ public class bulletWeapon : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("Shooting!");
             shoot();
         }
 
@@ -56,7 +59,8 @@ public class bulletWeapon : MonoBehaviour
         {
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
             Vector3 mouseRealtivePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            Debug.DrawLine(transform.position, mouseRealtivePosition, Color.red);
+            if (isMachinegun != true)
+                Debug.DrawLine(transform.position, mouseRealtivePosition, Color.red);
             drawAngle(fireSpread);
             setWeaponStats();
         }
@@ -90,14 +94,105 @@ public class bulletWeapon : MonoBehaviour
     {
         sumDeltaTime += Time.deltaTime;
         float secondsPerRound = 1.0f / fireRate;
-        Debug.Log(secondsPerRound);
 
         if (sumDeltaTime >= secondsPerRound)
         {
-            sumDeltaTime = 0;
-            GameObject bullet = Instantiate(bulletPref, transform.position, transform.rotation);
-            bullet.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed, ForceMode2D.Impulse);
+            //machinegun path logic
+            if (isMachinegun)
+            {
+                //odd streams
+                if (projectileCount % 2 == 1 && projectileCount != 1)
+                {
+                    float temp = projectileCount + 2;
+                    float rotationAngle = fireSpread / (temp-1);
+                    for (int i = 1; i < (temp - 1)/2; i++)
+                    {
+                        Vector3 posPoint = rotateVector(transform.right, rotationAngle * (i));
+                        Vector3 negPoint = rotateVector(transform.right, -rotationAngle * (i));
 
+                        GameObject bullet1 = Instantiate(bulletPref, transform.position, transform.rotation);
+                        GameObject bullet2 = Instantiate(bulletPref, transform.position, transform.rotation);
+                        GameObject bullet3 = Instantiate(bulletPref, transform.position, transform.rotation);
+
+                        bullet1.GetComponent<Rigidbody2D>().AddForce(posPoint * projectileSpeed, ForceMode2D.Impulse);
+                        bullet1.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+
+                        bullet2.GetComponent<Rigidbody2D>().AddForce(negPoint * projectileSpeed, ForceMode2D.Impulse);
+                        bullet2.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+
+                        bullet3.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed, ForceMode2D.Impulse);
+                        bullet3.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+                    }
+                }
+                //even streams
+                else if (projectileCount % 2 == 0)
+                {
+                    if (projectileCount > 2)
+                    {
+                        Vector3 point1 = rotateVector(transform.right, fireSpread/2);
+                        float rotationAngle = fireSpread / projectileCount;
+                        rotationAngle -= rotationAngle / projectileCount;
+                        float temp = projectileCount + 1;
+                        for (int i = 1; i < temp; i++)
+                        {
+                            Vector3 negPoint = rotateVector(point1, -rotationAngle * (i));
+                            GameObject bullet = Instantiate(bulletPref, transform.position, transform.rotation);
+                            bullet.GetComponent<Rigidbody2D>().AddForce(negPoint * projectileSpeed, ForceMode2D.Impulse);
+                            bullet.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+                        }
+                    }
+                    else
+                    {
+                        float rotationAngle = fireSpread / 5;
+                        Vector3 posPoint = rotateVector(transform.right, rotationAngle);
+                        Vector3 negPoint = rotateVector(transform.right, -rotationAngle);
+
+                        GameObject bullet1 = Instantiate(bulletPref, transform.position, transform.rotation);
+                        GameObject bullet2 = Instantiate(bulletPref, transform.position, transform.rotation);
+
+                        bullet1.GetComponent<Rigidbody2D>().AddForce(posPoint * projectileSpeed, ForceMode2D.Impulse);
+                        bullet1.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+
+                        bullet2.GetComponent<Rigidbody2D>().AddForce(negPoint * projectileSpeed, ForceMode2D.Impulse);
+                        bullet2.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+                    }
+                    
+                }
+                //single stream
+                else
+                {
+                    GameObject bullet = Instantiate(bulletPref, transform.position, transform.rotation);
+                    bullet.GetComponent<Rigidbody2D>().AddForce(transform.right * projectileSpeed, ForceMode2D.Impulse);
+                    bullet.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+                }
+            }
+            //shotgun path logic
+            else if (isShotgun)
+            {
+
+            }
+            //sniper path logic
+            else if (isSniper)
+            {
+
+            }
+            //basic gun logic
+            else
+            {
+                for (int i = 0; i < projectileCount; i++)
+                {
+                    //randomize vector direction to fire based on spread angle
+                    float randomAngle = Random.Range(-fireSpread/2, fireSpread/2);
+                    Vector3 fireVector = rotateVector(transform.right, randomAngle);
+
+                    GameObject bullet = Instantiate(bulletPref, transform.position, transform.rotation);
+                    bullet.GetComponent<Rigidbody2D>().AddForce(fireVector * projectileSpeed, ForceMode2D.Impulse);
+                    bullet.GetComponent<bulletProjectiles>().setStats(projectileRange, projectileDamage);
+                }
+            }
+
+            //set time to fire
+            sumDeltaTime = 0;
         }
     }
     // Debug functions
@@ -107,20 +202,61 @@ public class bulletWeapon : MonoBehaviour
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
         Vector3 mouseRealtivePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Vector3 point1 = new Vector3(
-            (Mathf.Cos(halfSpread) * mouseRealtivePosition.x) - (Mathf.Sin(halfSpread) * mouseRealtivePosition.y),
-            (Mathf.Sin(halfSpread) * mouseRealtivePosition.x) + (Mathf.Cos(halfSpread) * mouseRealtivePosition.y),
-            mouseRealtivePosition.z
-        );
-
-        Vector3 point2 = new Vector3(
-            (Mathf.Cos(-halfSpread) * mouseRealtivePosition.x) - (Mathf.Sin(-halfSpread) * mouseRealtivePosition.y),
-            (Mathf.Sin(-halfSpread) * mouseRealtivePosition.x) + (Mathf.Cos(-halfSpread) * mouseRealtivePosition.y),
-            mouseRealtivePosition.z
-        );
+        Vector3 point1 = rotateVector(mouseRealtivePosition, halfSpread);
+        Vector3 point2 = rotateVector(mouseRealtivePosition, -halfSpread);
 
         Debug.DrawLine(transform.position, point1, Color.yellow);
         Debug.DrawLine(transform.position, point2, Color.yellow);
+
+        //debug mode
+        if (isMachinegun)
+        {
+            //odd streams
+            if (projectileCount % 2 == 1 && projectileCount != 1)
+            {
+                projectileCount += 2;
+                float rotationAngle = fireSpread / (projectileCount-1);
+                for (int i = 1; i < (projectileCount - 1)/2; i++)
+                {
+                    Vector3 posPoint = rotateVector(mouseRealtivePosition, rotationAngle * (i));
+                    Vector3 negPoint = rotateVector(mouseRealtivePosition, -rotationAngle * (i));
+                    Debug.DrawLine(transform.position, posPoint, Color.red);
+                    Debug.DrawLine(transform.position, negPoint, Color.red);
+                    Debug.DrawLine(transform.position, mouseRealtivePosition, Color.red);
+                }
+            }
+            //even streams
+            else if (projectileCount % 2 == 0)
+            {
+                if (projectileCount > 2)
+                {
+                    float rotationAngle = fireSpread / projectileCount;
+                    rotationAngle -= rotationAngle / projectileCount;
+                    projectileCount += 1;
+                    for (int i = 1; i < projectileCount; i++)
+                    {
+                        Vector3 negPoint = rotateVector(point1, -rotationAngle * (i));
+                        Debug.DrawLine(transform.position, negPoint, Color.red);
+                    }
+                }
+                else
+                {
+                    float rotationAngle = fireSpread / 5;
+                    Vector3 posPoint = rotateVector(mouseRealtivePosition, rotationAngle);
+                    Vector3 negPoint = rotateVector(mouseRealtivePosition, -rotationAngle);
+
+                    Debug.DrawLine(transform.position, negPoint, Color.red);
+                    Debug.DrawLine(transform.position, posPoint, Color.red);
+                }
+                
+            }
+            //single stream
+            else
+            {
+                Debug.DrawLine(transform.position, mouseRealtivePosition, Color.red);
+            }
+        }
+
     }
     public void setWeaponStats()
     {
@@ -131,5 +267,16 @@ public class bulletWeapon : MonoBehaviour
         projectileDamage = float.Parse(input[3]);       
         projectileRange = float.Parse(input[4]);   
         projectileCount = float.Parse(input[5]);        
+    }
+
+    private Vector3 rotateVector(Vector3 vector, float angle)
+    {
+        Vector3 transVector = new Vector3(
+            vector.x * Mathf.Cos(angle) - vector.y * Mathf.Sin(angle),
+            vector.x * Mathf.Sin(angle) + vector.y * Mathf.Cos(angle),
+            vector.z
+        );
+
+        return transVector;
     }
 }
